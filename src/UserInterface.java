7458ffcc66a701369e40d003fd5cbbdc496d9800
Wellington Bragança <braganca.wellington@icloud.com>
2025-12-28 20:29:53 -0400

    Placing pieces

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,23 +1,28 @@
+import chess.ChessMatch;
 import chess.ChessPiece;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 public class UserInterface {
-    public static void printBoard (ChessPiece[][] pieces){
-        for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 -i) + " ");
-            for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j]);
-            }
+    public static void printBoard(ChessPiece[][] pieces){
+        IntStream.range(0, pieces.length).forEach(i -> {
+            System.out.print((8 - i) + " ");
+
+            Arrays.stream(pieces[i]).forEach(piece -> printPiece(piece));
+
             System.out.println();
-        }
+        });
         System.out.println("  A B C D E F G H");
     }
 
     private static void printPiece (ChessPiece piece){
-        if (piece == null){
-            System.out.print("-");
-        }else {
-            System.out.print(piece);
-        }
-        System.out.print(" ");
+        String output = Optional.ofNullable(piece)
+                .map(ChessPiece::toString)
+                .orElse("-");
+        System.out.print(output + " ");
     }
+
+
 }
