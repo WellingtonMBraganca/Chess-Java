68df49d138c082e0fc957e8e7ba6da751199ccd9
Wellington Bragança <braganca.wellington@icloud.com>
2026-01-09 17:12:21 -0400

    handling exception and crearing scream

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,9 @@
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -12,17 +14,27 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while(true){
-            UserInterface.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UserInterface.readChessPosition(sc);
+           try {
+               UserInterface.clearScreen();
+               UserInterface.printBoard(chessMatch.getPieces());
+               System.out.println();
+               System.out.print("Source: ");
+               ChessPosition source = UserInterface.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UserInterface.readChessPosition(sc);
+               System.out.println();
+               System.out.print("Target: ");
+               ChessPosition target = UserInterface.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.porformChessMove(source, target);
-
+               ChessPiece capturedPiece = chessMatch.porformChessMove(source, target);
+           }
+           catch (ChessException e ){
+               System.out.println(e.getMessage());
+               sc.nextLine();
+           }
+           catch (InputMismatchException e){
+               System.out.println(e.getMessage());
+               sc.nextLine();
+           }
         }
     }
 }
